48ebed3b141f0186621115768368e937167ce27a
Tomas Thor Jonsson <toj@valified.com>
2025-05-20 14:23:40 +0200

    Reduce logging severity for init timeout

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,7 @@
 import Config
 
 # Set log level to warn for tests to reduce noise
-config :logger, level: :warning
+config :logger, level: :info
 
 # Set the paths for testing customization
 config :mcp_sse, :sse_path, "/mcp/sse"
